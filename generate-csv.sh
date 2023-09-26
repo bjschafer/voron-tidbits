ae3bfb8a03e7a7aefdf06ed4981e36b863218f31
Braxton Schafer <braxton@cmdcentral.xyz>
2023-09-26 17:41:32 -0500

    feat: check for/use gawk; accept input path

--- generate-csv.sh
+++ generate-csv.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
+
+AWK='awk'
+
+if [[ "${OSTYPE}" == darwin* ]]; then
+    if command -v gawk &>/dev/null; then
+        AWK='gawk'
+    else
+        echo 'This script requires gawk (GNU awk) to run and you seem to not have it.'
+        echo 'If you are doing funky stuff, you know better than I do, just remove this check.'
+        exit 1
+    fi
+fi
 
 # Base directory that contains STL files
-BASE='./VT/STLs'
+BASE="$1"
 
 # CSV header
 echo 'partname,component,subcomponent,color,quantity,filepath'
 
 while read -r -d '' stlPath ; do
-    stlPath="${stlPath##${BASE}/}" # strip $BASE from the path
-    awk -F'/' -f process-path.awk <<< "${stlPath}"
+    stlPath="${stlPath##"${BASE}/"}" # strip $BASE from the path
+    "${AWK}" -F'/' -f process-path.awk <<< "${stlPath}"
 done < <(find "${BASE}" -type f -name '*.stl' -print0)
